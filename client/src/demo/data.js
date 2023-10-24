9461a2588d7fc4d5014ca6acdd8168ca39136e24
chopper <89061191+Cchopksd@users.noreply.github.com>
2023-10-24 13:55:59 +0700

    create demo json

---
+++ client/src/demo/data.js
@@ -0,0 +1,10 @@
+export const stockData = [
+    {
+      // img1: '../assets/images/newjean1.jpg',
+      // img2: '../assets/images/newjean2.jpg',
+      // img3: '../assets/images/newjean3.jpg',
+      name: 'ยุรนันท์ เจิศจุริกุล (ใจทราม)',
+      date: 'วันที่ที่หาย: 19 พฤศจิกายน 2566',
+      status: 'ยังไม่พบ'
+    },
+]
