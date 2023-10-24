226169e64baf4af41c29945c995fdfc7afc563e0
chopper <89061191+Cchopksd@users.noreply.github.com>
2023-10-24 12:43:10 +0700

    create router

---
+++ client/src/pages/user/FindMissing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FindMissing = () => {
+    return (
+        <div>
+        <p style={{color:"black"}}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, culpa non?
+            Exercitationem numquam labore rem ipsum mollitia eum quo, tempore et
+            neque, cupiditate fugit incidunt sunt deleniti quas nesciunt magnam.
+        </p>
+        </div>
+    );
+};
+
+export default FindMissing;
---
+++ client/src/pages/user/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
+                consectetur voluptatum voluptatem a velit quod explicabo repellendus nemo
+                aspernatur natus voluptas modi ab eius, repudiandae asperiores ad!
+                Expedita, perspiciatis commodi?
+            </p>
+        </div>
+    );
+};
+
+export default Login;
