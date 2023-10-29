6c4bf1ea37404d0946c637ebd4ac21331144b6ef
singto243 <89060995+singto243@users.noreply.github.com>
2023-10-29 19:03:18 +0700

    อัพเดทหน้า Clue Component (ยังไม่เสร็จ)

--- client/src/components/user/MissingClue.jsx
+++ client/src/components/user/MissingClue.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { stockData } from '../../demo/data';
+import './css/MissingClue.css'
+import { IoSend } from 'react-icons/io5'
+import { GrCamera } from 'react-icons/gr'
+import { BsDot } from 'react-icons/bs'
 
 const MissingClue = () => {
+    const dummyData = [
+        {
+            fname : 'กนกพล',
+            lname : 'เทียมเมือง',
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus eque ac faucibus porta, nulla augue rutrum orci, et imperdiet erat ex et massa Vestibulum mattis, purus eget blandit tempus, mauris turpis dignissim lorem, ac fermentum est ligula porta est. Vestibulum congue, nisi ut consequat eleifend, urna arcu varius ligula.',
+            date : '21 พฤศจิกายน 2566',
+            profilePhoto : 'https://dudeproducts.com/cdn/shop/articles/gigachad_1068x.jpg?v=1667928905',
+            cluePhoto1 : 'https://www.thai2night.com/upload/shop/photo_cover/master/20180323235749M5hkkI5P9P.jpg',
+            cluePhoto2 : "",
+            cluePhoto3 : ""
+        },
+    ]
     return (
-        <div>
-            <hr />
-            {stockData.map((data, key) => {
-                <div>
-                    
+        <div className='missClue-container'>
+            <hr/>
+            <div className='missClue-writeComment-box'>
+                <img className='missClue-login-photo' src="https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg"/>
+                <div className='missClue-input-box'>
+                    <textarea className='missClue-input' placeholder='คุณมีเบาะแสหรอ? มาแบ่งปันกันเถอะ...'/>
+                    <div className='missClue-input-footer-box'>
+                        <GrCamera size={20}/>
+                        <IoSend size={20}/>
+                    </div>
                 </div>
-            })}
+            </div>
+            <div className='missClue-comment-box'>
+                <hr/>
+                <div className='missClue-comment-header'>
+                    <img className='missClue-comment-profile-photo' src="https://dudeproducts.com/cdn/shop/articles/gigachad_1068x.jpg?v=1667928905"/>
+                    <label className='missClue-comment-name'>กนกพล</label>
+                    <label className='missClue-comment-name'>เทียมเจียว</label>
+                    <BsDot size={30}/>
+                    <label className='missClue-comment-date'>21 พฤศจิกายน 2566</label>
+                </div>
+                <div className='missClue-comment-des-box'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus eque ac faucibus porta, nulla augue rutrum orci, et imperdiet erat ex et massa Vestibulum mattis, purus eget blandit tempus, mauris turpis dignissim lorem, ac fermentum est ligula porta est. Vestibulum congue, nisi ut consequat eleifend, urna arcu varius ligula.</p>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/pages/user/MissingProfile.jsx
+++ client/src/pages/user/MissingProfile.jsx
@@ -16,58 +16,60 @@ const MissingProfile = () => {
     }
 
     return (
-        <div className="missing-profile-page">
-            {/* <Navbar/> */}
-            <div className="missing-profile-container">
-                {stockData.map((data, key) => {
-                    return (
-                        <div className="missing-profile-container-center" key={key}>
-                            <main className="missing-profile-header" >
-                                <div className="image-missing-group">
-                                    <img className="image-missing-profile" src={data.img1} alt="" />
-                                    <img className="image-missing-profile" src={data.img2} alt="" />
-                                    <img className="image-missing-profile" src={data.img3} alt="" />
-                                </div>
-                                <div className="missing-profile-body">
-                                    <div>
-                                        <div className="missing-profile-body-name">
-                                            {data.name}
+        <div>
+            <Navbar/>
+            <div className="missing-profile-page">
+                {/* <Navbar/> */}
+                <div className="missing-profile-container">
+                    {stockData.map((data, key) => {
+                        return (
+                            <div className="missing-profile-container-center" key={key}>
+                                <main className="missing-profile-header" >
+                                    <div className="image-missing-group">
+                                        <img className="image-missing-profile" src={data.img1} alt="" />
+                                        <img className="image-missing-profile" src={data.img2} alt="" />
+                                        <img className="image-missing-profile" src={data.img3} alt="" />
+                                    </div>
+                                    <div className="missing-profile-body">
+                                        <div>
+                                            <div className="missing-profile-body-name">
+                                                {data.name}
+                                            </div>
+                                            <div className="missing-profile-body-date">
+                                                {data.date}
+                                            </div>
                                         </div>
-                                        <div className="missing-profile-body-date">
-                                            {data.date}
+                                        <div className={`missing-profile-body-status
+                                            ${data.status ? 'found' : 'not-found'}
+                                        `}>
+                                            {data.status ? 'พบแล้ว' : 'ยังไม่พบ'}
                                         </div>
                                     </div>
-                                    <div className={`missing-profile-body-status
-                                        ${data.status ? 'found' : 'not-found'}
-                                    `}>
-                                        {data.status ? 'พบแล้ว' : 'ยังไม่พบ'}
-                                    </div>
-                                </div>
-                            </main>
-                            <div className="missing-profile-button-group">
-                                <button
-                                    className={`missing-profile-button-switch button-info
-                                        ${activeButton === 'button-info' ? 'clicked' : ''}
+                                </main>
+                                <div className="missing-profile-button-group">
+                                    <button
+                                        className={`missing-profile-button-switch button-info
+                                            ${activeButton === 'button-info' ? 'clicked' : ''}
+                                        `}
+                                        onClick={() => handleClicked('button-info')}
+                                    >
+                                        ข้อมูล
+                                    </button>
+                                    <button
+                                        className={`missing-profile-button-switch button-clue
+                                            ${activeButton === 'button-clue' ? 'clicked' : ''}
                                     `}
-                                    onClick={() => handleClicked('button-info')}
-                                >
-                                    ข้อมูล
-                                </button>
-                                <button
-                                    className={`missing-profile-button-switch button-clue
-                                        ${activeButton === 'button-clue' ? 'clicked' : ''}
-                                `}
-                                    onClick={() => handleClicked('button-clue')}
-                                >
-                                    เบาะแส
-                                </button>
+                                        onClick={() => handleClicked('button-clue')}
+                                    >
+                                        เบาะแส
+                                    </button>
+                                </div>
+                                {activeButton === 'button-info' ? <MissingInfo /> : <MissingClue />}
                             </div>
-                            {activeButton === 'button-info' ? <MissingInfo /> : <MissingClue />}
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-            <Footer/>
         </div>
     );
 };
