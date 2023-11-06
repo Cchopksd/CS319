79d66dd7e69b8c5a535240fe38f93492ab643c29
chopper <89061191+Cchopksd@users.noreply.github.com>
2023-11-06 22:24:03 +0700

    Update AdminSinglePage.jsx

--- client/src/pages/admin/AdminSinglePage.jsx
+++ client/src/pages/admin/AdminSinglePage.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import { IoIosArrowBack } from 'react-icons/io';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const AdminSinglePage = () => {
+
+    const params = useParams();
+
+    const [ missingData, setMissingData ] = useState('')
+    console.log(missingData)
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_APP_API}/admin/${(params.slug)}`)
+        .then((response) =>{
+            setMissingData(response.data)
+        })
+        .catch((error) =>{
+            alert(error.message);
+        })
+    }, [params.slug])
 
     const navigate = useNavigate()
 
@@ -14,10 +30,13 @@ const AdminSinglePage = () => {
     return (
         <div>
             <Navbar />
-            <main>
-                <div className="find-back-box" onClick={handleBack}>
+            <div className="find-back-box" onClick={handleBack}>
                     <IoIosArrowBack size={60} />
                 </div>
+            <main>
+                <img className='' src={missingData.missing_photo1} alt="" />
+                <img src={missingData.missing_photo2} alt="" />
+                <img src={missingData.missing_photo3} alt="" />
             </main>
             <Footer />
         </div>
