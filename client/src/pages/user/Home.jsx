a00c75d9a544dcb252766d65c9f1f4970a7faccd
singto243 <89060995+singto243@users.noreply.github.com>
2023-11-09 13:32:27 +0700

    link ให้คลิกที่ profile แล้วไปหน้า profile

--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useContext} from "react";
 import './css/Home.css'
 import { HiOutlinePhone } from 'react-icons/hi'
 import { MdOutlineModeComment } from 'react-icons/md'
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import axios from 'axios'
@@ -134,10 +134,6 @@ const Home = () => {
         navigate(`/find-missing`)
     }
 
-    const handleGoProfile = () => {
-        navigate(`/missing-profile`)
-    }
-
     return (
         <>  
             <Navbar/>
@@ -172,45 +168,47 @@ const Home = () => {
                 <label className="home-missing-title">ผู้สูญหายกรณีล่าสุด</label>
                 <div className="home-missing-latest-box">
                 {postArray.map((item) => (
-                            <div className="home-card" key={item._id} onClick={handleGoProfile}>
-                                <img src={item.missing_photo1} alt=""/>
-                                <div className="home-info-box">
-                                    <div className="home-card-row-1">
-                                        <label>{formatDate(item.updatedAt)}</label>
-                                        <div className="home-card-status">
-                                            {item.missing_status}
-                                        </div>
-                                    </div>
-                                    <div className="home-card-row-2">
-                                        <label>{item.missing_fname}&ensp;</label>
-                                        <label>{item.missing_lname}&ensp;</label>
-                                        <label>{`(${item.missing_gender})`}</label>
-                                    </div>
-                                    <div className="home-card-row-3">
-                                        <div className="home-card-place">
-                                            <label>สูญหายที่ :</label>
-                                            <label> {item.missing_position.length > 15 ? item.missing_position.slice(0, 15) + "..." : item.missing_position}</label>
-                                        </div>
-                                        <div className="home-card-cause">
-                                            <label>สาเหตุการหาย :</label>
-                                            <label> {item.missing_cause.length > 15 ? item.missing_cause.slice(0, 15) + "..." : item.missing_cause}</label>
-                                        </div>
-                                        <div className="home-card-date">
-                                            <label>วันที่รายงานการสูญหาย :</label>
-                                            <label> {formatDate(item.createdAt)}</label>
-                                        </div>
-                                    </div>
-                                    <div className="home-card-row-4">
-                                        <div className="home-card-province">
-                                            {item.missing_province}
-                                        </div>
-                                        <div className="home-card-clue">
-                                            <MdOutlineModeComment size={20} className="home-card-clue-icon"/>
-                                            20
-                                        </div>
+                    <Link to={`/missing-profile/${item.missing_slug}`} style={{textDecoration: 'none', color: 'inherit'}}>
+                        <div className="home-card" key={item._id}>
+                            <img src={item.missing_photo1} alt=""/>
+                            <div className="home-info-box">
+                                <div className="home-card-row-1">
+                                    <label>{formatDate(item.updatedAt)}</label>
+                                    <div className="home-card-status">
+                                        {item.missing_status}
                                     </div>
                                 </div>
+                                <div className="home-card-row-2">
+                                    <label>{item.missing_fname}&ensp;</label>
+                                    <label>{item.missing_lname}&ensp;</label>
+                                    <label>{`(${item.missing_gender})`}</label>
+                                </div>
+                                <div className="home-card-row-3">
+                                    <div className="home-card-place">
+                                        <label>สูญหายที่ :</label>
+                                        <label> {item.missing_position.length > 15 ? item.missing_position.slice(0, 15) + "..." : item.missing_position}</label>
+                                    </div>
+                                    <div className="home-card-cause">
+                                        <label>สาเหตุการหาย :</label>
+                                        <label> {item.missing_cause.length > 15 ? item.missing_cause.slice(0, 15) + "..." : item.missing_cause}</label>
+                                    </div>
+                                    <div className="home-card-date">
+                                        <label>วันที่รายงานการสูญหาย :</label>
+                                        <label> {formatDate(item.createdAt)}</label>
+                                    </div>
+                                </div>
+                                <div className="home-card-row-4">
+                                    <div className="home-card-province">
+                                        {item.missing_province}
+                                    </div>
+                                    <div className="home-card-clue">
+                                        <MdOutlineModeComment size={20} className="home-card-clue-icon"/>
+                                        20
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                    </Link>
                         ))}
                 </div>
             </div>
